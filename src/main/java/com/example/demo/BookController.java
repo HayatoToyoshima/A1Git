2144eb449853001da22e2733289389c33324ba32
Toyoshima Hayato <hayato.toyoshima@g.dreamcareer.co.jp>
2024-05-21 14:29:51 +0900

    commit!

---
+++ src/main/java/com/example/demo/BookController.java
@@ -0,0 +1,67 @@
+package com.example.demo;
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public String getAllBooks(Model model) {
+        List<Book> books = bookService.getAllBooks();
+        model.addAttribute("books", books);
+        model.addAttribute("book", new Book());
+        return "books";
+    }
+
+    @PostMapping
+    public String createBook(@ModelAttribute Book book) {
+        bookService.saveBook(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable("id") Long id, Model model) {
+        Book book = bookService.getBookById(id).orElse(new Book());
+        model.addAttribute("book", book);
+        return "books";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateBook(@PathVariable("id") Long id, @ModelAttribute Book bookDetails) {
+        bookService.getBookById(id).ifPresent(book -> {
+            book.setTitle(bookDetails.getTitle());
+            book.setAuthor(bookDetails.getAuthor());
+            book.setIsbn(bookDetails.getIsbn());
+            bookService.saveBook(book);
+        });
+        return "redirect:/books";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id) {
+        bookService.deleteBook(id);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/clock")
+    public String getClock() {
+    	return "clock";
+    }
+    
+    
+}
